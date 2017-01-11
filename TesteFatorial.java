ac87b74db970f638155a4445381dccfa0b2e263f
julio-git <juliodisouza@hotmail.com>
2017-01-10 22:30:34 -0200

    Calculo do fatorial de 10

--- TesteFatorial.java
+++ TesteFatorial.java
@@ -2,8 +2,8 @@ public class TesteFatorial {
 	// Comentario inserido em TesteFatorial.java
 	public static void main(String [] args){
 		Fatorial fat = new Fatorial();
-		int resultado = fat.calcularFatorial(5);
-		System.out.println(resultado);
+		int r = fat.calcularFatorial(10);
+		System.out.println(r);
 	}
 
 }
